c6686bc7e25f1da9e43d84255db1c24ef3190397
Youngho Cho <dominoyh5@gmail.com>
2020-10-31 00:00:04 +0900

    draw baduk board line

--- static/test.js
+++ static/test.js
@@ -1,2 +1,27 @@
-let canvas = document.getElementById('canvas');
-let ctx = canvas.getContext('2d');
+const height = 900;
+const width = 900;
+const offset = 30;
+const num_of_line = 19;
+
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "rgb(210,165,73)";
+ctx.fillRect(0, 0, width, height);
+
+ctx.fillStyle = "rgb(0,0,0)";
+for (
+  let i = offset;
+  i <= width - offset + offset * 0.4;
+  i += (width - offset * 2) / (num_of_line -1)
+) {
+  ctx.beginPath();
+  ctx.moveTo(i, offset);
+  ctx.lineTo(i, width - offset);
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.moveTo(offset, i);
+  ctx.lineTo(height - offset, i);
+  ctx.stroke();
+}
